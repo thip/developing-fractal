929a552d35d2b2c8fc48fe32bef890e770653330
thip <david@thecappers.co.uk>
2016-11-03 18:03:23 +0000

    experimenting with layering

--- Main.hs
+++ Main.hs
@@ -9,29 +9,29 @@ data Angle = Angle Float deriving (Show)
 data LSystem = LSystem Angle Axiom [Rule] deriving (Show)
 
 segLength = 10
-iterations = 6
+iterations = 7
 
 dragon = LSystem (Angle (pi/2)) (Axiom "f") [Rule 'f' "f-h", Rule 'h' "f+h"]
 terDragon = LSystem (Angle (2*pi/3)) (Axiom "f") [Rule 'f' "f+f-f"]
 
 main :: IO()
-main = display (InWindow "fractal" (300, 300) (100, 100)) (white) (drawSmoothFractal terDragon iterations)
+main = display (InWindow "fractal" (300, 300) (100, 100)) (black) (pictures [Color red $  drawSmoothFractal terDragon (Angle 0) segLength iterations, Color blue $  drawSmoothFractal terDragon (Angle (pi/6)) (segLength*1.7320508) $ iterations-1, Color green $  drawSmoothFractal terDragon (Angle (2*pi/6)) (segLength*1.7320508*1.7320508) $ iterations-2])
 
 
-drawSmoothFractal :: LSystem->Int->Picture
-drawSmoothFractal (LSystem (Angle theta) (Axiom a) rules) n = line $ smooth ([(0,0)] ++ makePath (iterate' n a rules) theta (0,0) 0) 16
+drawSmoothFractal :: LSystem->Angle->Float->Int->Picture
+drawSmoothFractal (LSystem (Angle theta) (Axiom a) rules) (Angle offset) segmentLength  n = line $ smooth ([(0,0)] ++ makePath (iterate' n a rules) (theta) (0,0) offset segmentLength) 16 
 
 
 drawFractal :: LSystem->Int->Picture
-drawFractal (LSystem (Angle theta) (Axiom a) rules) n = line ([(0,0)] ++ makePath (iterate' n a rules) theta (0,0) 0)
+drawFractal (LSystem (Angle theta) (Axiom a) rules) n = line ([(0,0)] ++ makePath (iterate' n a rules) theta (0,0) 0 segLength)
 
-makePath :: String->Float->Vector->Float->Path
-makePath [] theta curPos initialTheta = []
-makePath (c:cs) theta curPos initialTheta
-        | c == '+' = makePath cs theta curPos (initialTheta + theta)
-        | c == '-' = makePath cs theta curPos (initialTheta - theta)
-        | otherwise = [newPos] ++ (makePath cs theta newPos initialTheta)
-                where newPos =  curPos + (segLength * (sin initialTheta), segLength * (cos initialTheta))
+makePath :: String->Float->Vector->Float->Float->Path
+makePath [] theta curPos initialTheta l= []
+makePath (c:cs) theta curPos initialTheta l
+        | c == '+' = makePath cs theta curPos (initialTheta + theta) l
+        | c == '-' = makePath cs theta curPos (initialTheta - theta) l
+        | otherwise = [newPos] ++ (makePath cs theta newPos initialTheta l)
+                where newPos =  curPos + (l * (sin initialTheta), l * (cos initialTheta))
 
 iterate' :: Int->String->[Rule]->String
 iterate' 0 string rules = string
