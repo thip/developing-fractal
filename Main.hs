eddaa5f7c0716d6743f135dfc902ceaf6c2a8300
thip <david@thecappers.co.uk>
2017-05-31 19:44:15 +0100

    killed some magic numbers and abstracted some stuff away

--- Main.hs
+++ Main.hs
@@ -1,6 +1,7 @@
 module Main(main) where
 
 import Graphics.Gloss
+import Graphics.Gloss.Data.Vector
 import Bezier
 
 data Rule = Rule Char String deriving (Show)
@@ -10,22 +11,29 @@ data LSystem = LSystem Angle Axiom [Rule] deriving (Show)
 
 segLength = 10
 iterations = 2
-sf = tan (pi/3)
+
+chosenSystem = terDragon
+twist = (angleAsFloat . getTwistAngle) chosenSystem
+    where angleAsFloat (Angle a) = a
+
+sf = getScaleFactor chosenSystem
 
 dragon = LSystem (Angle (pi/2)) (Axiom "f") [Rule 'f' "f-h", Rule 'h' "f+h"]
 terDragon = LSystem (Angle (2*pi/3)) (Axiom "f") [Rule 'f' "f+f-f"]
 
 main :: IO()
-main = display (InWindow "fractal" (300, 300) (100, 100)) (white) (Pictures (scaleAndRotateLayers (drawIterations 0 8 terDragon)))
-
+main = do 
+    print (getScaleFactor chosenSystem)
+    display (InWindow "fractal" (300, 300) (100, 100)) (white) (Pictures (scaleAndRotateLayers (drawIterations 0 8 chosenSystem)))
 
 scaleAndRotateLayers :: [Picture] -> [Picture]
 scaleAndRotateLayers layers = scaleAndRotateLayers' (reverse layers)  0 
 
 scaleAndRotateLayers' :: [Picture]->Int->[Picture]
 scaleAndRotateLayers' [] layerNumber = []
-scaleAndRotateLayers' (layer:layers) layerNumber = [(scaleAndRotate (sf**n) (n*pi/6) layer)] ++ (scaleAndRotateLayers' layers (layerNumber+1))
+scaleAndRotateLayers' (layer:layers) layerNumber = [(scaleAndRotate (sf**n) (n*a) layer)] ++ (scaleAndRotateLayers' layers (layerNumber+1))
     where n = fromIntegral layerNumber  
+          a = twist
 
 scaleAndRotate :: Float->Float->Picture->Picture
 scaleAndRotate scaleFac angle picture = scale scaleFac scaleFac (rotate' angle picture)
@@ -37,18 +45,21 @@ rotate' :: Float->Picture->Picture
 rotate' angle picture = rotate (angle * (180/pi)) picture
 
 drawSmoothFractal :: LSystem->Int->Picture
-drawSmoothFractal (LSystem (Angle theta) (Axiom a) rules) n = Color (greyN ( ( fromIntegral n )/10)) $  line $ smooth 32 ([(0,0)] ++ makePath (iterate' n a rules) theta (0,0) 0) 
+drawSmoothFractal system n = Color (greyN (0.01+ ( fromIntegral n )/10)) $  line $ smooth 32 ([(0,0)] ++ makePath system n) 
 
 
 drawFractal :: LSystem->Int->Picture
-drawFractal (LSystem (Angle theta) (Axiom a) rules) n = line ([(0,0)] ++ makePath (iterate' n a rules) theta (0,0) 0)
+drawFractal system n = line ([(0,0)] ++ makePath system n)
 
-makePath :: String->Float->Vector->Float->Path
-makePath [] theta curPos initialTheta = []
-makePath (c:cs) theta curPos initialTheta
-        | c == '+' = makePath cs theta curPos (initialTheta + theta)
-        | c == '-' = makePath cs theta curPos (initialTheta - theta)
-        | otherwise = [newPos] ++ (makePath cs theta newPos initialTheta)
+makePath :: LSystem->Int->Path
+makePath (LSystem (Angle theta) (Axiom a) rules) n  = makePath' (iterate' n a rules) theta (0,0) 0
+
+makePath' :: String->Float->Vector->Float->Path
+makePath' [] theta curPos initialTheta = []
+makePath' (c:cs) theta curPos initialTheta
+        | c == '+' = makePath' cs theta curPos (initialTheta + theta)
+        | c == '-' = makePath' cs theta curPos (initialTheta - theta)
+        | otherwise = [newPos] ++ (makePath' cs theta newPos initialTheta)
                 where newPos =  curPos + (segLength * (sin initialTheta), segLength * (cos initialTheta))
 
 iterate' :: Int->String->[Rule]->String
@@ -66,3 +77,17 @@ replace' c ((Rule c' s):[])
 replace' c ((Rule c' s):rs)
         | c == c'   = s
         | otherwise = replace' c rs 
+
+getScaleFactor :: LSystem->Float
+getScaleFactor system = (magV delta1)/(magV delta0)
+    where delta0 = (last path0)
+          delta1 = (last path1)
+          path0 = makePath system 0
+          path1 = makePath system 1
+
+getTwistAngle :: LSystem->Angle
+getTwistAngle system  = Angle ((argV sum1) - (argV sum0))
+    where sum0 = (last path0) - (head path0)
+          sum1 = (last path1) - (head path1)
+          path0 = makePath system 0
+          path1 = makePath system 1
