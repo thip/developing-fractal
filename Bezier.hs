8a3615ee5d3e445bcfacf7d78e3e82c5221371dc
thip <david@thecappers.co.uk>
2017-06-07 07:35:31 +0100

    outputting a jagged mesh, need some refactoring

--- Bezier.hs
+++ Bezier.hs
@@ -7,41 +7,42 @@ import Data.List
 type Section = (Point, Point, Point)
 
 smooth :: Integer -> [Point] -> [Point]
-smooth n path = [(0,0)] ++ smooth' n path
+smooth n path = (0,0) : smooth' n path
 
 smooth' :: Integer -> [Point] -> [Point]
-smooth' n (a:[]) = [a]
-smooth' n (a:b:[]) = interpolateLine a b (n)
-smooth' n (a:b:c:[]) = interpolateSection (a, c, b) (n*2)
+smooth' n [a] = [a]
+smooth' n [a,b] = interpolateLine a b n
+smooth' n (a:b:ps) = interpolateLine a b n ++ smooth' n (b:ps)
+--smooth' n (a:b:c:[]) = interpolateSection (a, c, b) (n*2)
 --smooth' n (a:b:c:d:[]) = (interpolateSection (a, (half b c), b) (n*2) )  ++ (interpolateSection ((half b c), d, c) n )
-smooth' n (a:b:c:d:e:[]) = (interpolateSection (a, (half b c), b) (n*2)) ++ c ++ (interpolateSection ((half c d), e, d) (n*2))
-smooth' n (a:b:c:ps) = (smoothEnd n a b c) ++  (smoothMiddle n middle) ++ (smoothOtherEnd n end)
-    where middle = [b] ++ [c] ++ reverse (  drop 1 ( reverse ps))
-          end  = reverse ( take 3 ( reverse ps))
+--smooth' n (a:b:c:d:e:[]) = (interpolateSection (a, (half b c), b) (n*2)) ++ [c] ++ (interpolateSection ((half c d), e, d) (n*2))
+--smooth' n (a:b:c:ps) = (smoothEnd n a b c) ++  (smoothMiddle n middle) ++ (smoothOtherEnd n end)
+--    where middle = [b] ++ [c] ++ reverse (  drop 1 ( reverse ps))
+--          end  = reverse ( take 3 ( reverse ps))
 
 smoothEnd :: Integer -> Point -> Point -> Point -> [Point]
-smoothEnd n a b c = interpolateSection (a, (half b c), b) (n*2+1)
+smoothEnd n a b c = interpolateSection (a, half b c, b) (n*2+1)
 
 smoothOtherEnd :: Integer -> [Point] -> [Point]
-smoothOtherEnd n (a:b:c:[]) = interpolateSection ((half a b), c, b) (n*2+1)
-smoothOtherEnd n (a:b:[]) = interpolateLine (half a b) b (n+1)
-smoothOtherEnd n (a:[]) = [a]
+smoothOtherEnd n [a,b,c] = interpolateSection (half a b, c, b) (n*2+1)
+smoothOtherEnd n [a,b] = interpolateLine (half a b) b (n+1)
+smoothOtherEnd n [a] = [a]
 
 smoothMiddle :: Integer -> [Point] -> [Point]
 smoothMiddle n [] = []
 --smoothMiddle n (a:[]) = [a]
-smoothMiddle n (a:b:[]) = interpolateLine a b (n+1)
-smoothMiddle n (a:b:c:[]) = interpolateSection ((half a b), (half b c), b) (n*2+1)
-smoothMiddle n (a:b:c:ps) = (smoothMiddle n ([a]++[b]++[c]))  ++ (smoothMiddle n ([b] ++ [c] ++ ps)) 
+smoothMiddle n [a,b] = interpolateLine a b (n+1)
+smoothMiddle n [a,b,c] = interpolateSection (half a b, half b c, b) (n*2+1)
+smoothMiddle n (a:b:c:ps) = smoothMiddle n ([a]++[b]++[c])  ++ smoothMiddle n ([b] ++ [c] ++ ps) 
 
 half :: Point -> Point -> Point
-half a b = a + (mulSV 0.5 (b-a)) 
+half a b = a + mulSV 0.5 (b-a) 
 
 interpolateLine :: Point -> Point -> Integer -> [Point]
-interpolateLine a b n = [ pointAlongLine a b (x/(fromIntegral n)) | x <- [ fromIntegral x' | x' <- [1..n] ] ]
+interpolateLine a b n = [ pointAlongLine a b (x/fromIntegral n) | x <- [ fromIntegral x' | x' <- [1..n] ] ]
 
 interpolateSection :: Section -> Integer -> [Point]
-interpolateSection section n = [ point section (x/(fromIntegral n)) | x <- [ fromIntegral x' | x' <- [1..n] ] ]
+interpolateSection section n = [ point section (x/fromIntegral n) | x <- [ fromIntegral x' | x' <- [1..n] ] ]
 
 point :: Section -> Float -> Point
 point (start, end, handle) n = pointAlongLine s e n
@@ -49,6 +50,6 @@ point (start, end, handle) n = pointAlongLine s e n
               e = pointAlongLine handle end n 
 
 pointAlongLine :: Point -> Point -> Float -> Point
-pointAlongLine a b d = a + (mulSV  d  (b-a))
+pointAlongLine a b d = a + mulSV  d  (b-a)
 
 -- 3(n-1) +1
--- Main.hs
+++ Main.hs
@@ -1,72 +1,97 @@
 module Main(main) where
 
+import Text.Printf
 import Graphics.Gloss
 import Graphics.Gloss.Data.Vector
 import Bezier
 
 data Rule = Rule Char String deriving (Show)
-data Axiom = Axiom String deriving (Show)
-data Angle = Angle Float deriving (Show)
+newtype Axiom = Axiom String deriving (Show)
+newtype  Angle = Angle Float deriving (Show)
 data LSystem = LSystem Angle Axiom [Rule] deriving (Show)
 
 chosenSystem = terDragon
 startIterations = 0
-endIterations =2
+endIterations = 4
 
 
 dragon = LSystem (Angle (pi/2)) (Axiom "f") [Rule 'f' "f-h", Rule 'h' "f+h"]
 terDragon = LSystem (Angle (2*pi/3)) (Axiom "f") [Rule 'f' "f+f-f"]
 
 main :: IO()
-main = do
-    mapM_ (print) (map length (map (smooth 3) lists))
-    mapM_ (print) lists
-    where lists = [map (\a->(a,a)) [0..n] | n <- [1..10]]
---    mapM_ (print) (map length alignedPaths)
---    display  (InWindow "fractal" (300, 300) (100, 100)) (white) (Pictures layers)
---        where layers = (map line alignedPaths) -- ++ map markPoints smoothPaths
---              --smoothPaths = (map (\(n, path) -> (smooth (2^n) path)) (addIndicies (alignedPaths)))
---              alignedPaths = scaleAndRotateCurves chosenSystem paths'
---              --paths = map ((scale' 10).(makePath chosenSystem)) [startIterations..endIterations]
---	      paths' = [((smooth 3) ^* ((endIterations-n))) ((makePath chosenSystem (n+2))) | n <- [startIterations..endIterations] ]   
+main = -- do
+
+--    mapM_ (print) lists
+--    where lists = [map (\a->(a,a)) [0..n] | n <- [1..10]]
+    --mapM_ (print) (map length alignedPaths)
+    --display  (InWindow "fractal" (300, 300) (100, 100)) (white) (Pictures layers)
+    putStrLn (makeObj ( map (scale' 10) alignedPaths) ++ faces alignedPaths)
+        where layers = map line alignedPaths ++ map markPoints alignedPaths
+              --smoothPaths = (map (\(n, path) -> (smooth (2^n) path)) (addIndicies (alignedPaths)))
+              alignedPaths = scaleAndRotateCurves chosenSystem paths'
+              --paths = map ((scale' 10).(makePath chosenSystem)) [startIterations..endIterations]
+              paths' = [(smooth 3 ^* (endIterations-n)) (makePath chosenSystem n) | n <- [startIterations..endIterations] ]   
+
+showFloat :: Float->String
+showFloat = printf "%.10f"
+
+makeObj :: [[Vector]] -> String
+makeObj curves = (concat.concat) $ map 
+                                    (\(order, curve) 
+                                        -> (map (makeVectorString . addZ (1.5*3^order)) curve )) 
+                                    (zip [0..] curves)
+
+makeVectorString ::  (Float,Float,Float)->String
+makeVectorString (x,y,z) = printf "v %0.10f %0.10f %0.10f\n" x y z
+
+addZ :: Float -> (Float, Float) -> (Float, Float,Float)
+addZ z (x,y) = (x, y, z)
+
+faces :: [[Vector]]->String
+faces (c:curves) = concat [ printf "f %d %d %d\nf %d %d %d\n" a b c b d c  | (a,b,c,d) <- squares] 
+    where squares = [(p, p+1, p+width, p+width+1) | p <- topCorners]
+          topCorners = [ x+y  | x  <- [0..(width-2)], y <- layerStarts ] 
+          layerStarts = [ 1+n*width | n  <- [0..(height-2)]]
+          height = 1 + length curves 
+          width = length c
 
 (^*) f n = selfComposeN n f 
 
 selfComposeN :: Integer -> (a->a) -> a -> a
 selfComposeN 0 function arg = function arg
-selfComposeN n function arg = function (selfComposeN (n-1) (function) (arg))
+selfComposeN n function arg = function (selfComposeN (n-1) function arg)
 
 markPoints :: [Vector] -> Picture
 markPoints points = Pictures (map drawCircle points)
     where drawCircle (x,y) = translate x y (Circle 0.1)
 
 scaleAndRotateCurves :: LSystem->[[Vector]]->[[Vector]]
-scaleAndRotateCurves system curves = map (\(n, curve)->scaleAndRotate (scaleFactor**(fromIntegral n)) (angle*(fromIntegral n)) curve) (addIndicies (reverse curves)) 
+scaleAndRotateCurves system curves = map (\(n, curve)->scaleAndRotate (scaleFactor**fromIntegral n) (angle*fromIntegral n) curve) (addIndicies (reverse curves)) 
     where angle = -( ( \(Angle a) -> a ) . getTwistAngle) system -- -ve because we've reversed the order of the curves
           scaleFactor = getScaleFactor system
 
 addIndicies :: [a]->[(Integer, a)]
-addIndicies xs = zip [0..] xs
+addIndicies = zip [1..]
 
 scaleAndRotate :: Float->Float->[Vector]->[Vector]
-scaleAndRotate scaleFactor angle points = ((scale' scaleFactor) . (rotate' angle)) points
+scaleAndRotate scaleFactor angle = scale' scaleFactor . rotate' angle
 
 scale' :: Float->[Vector]->[Vector]
-scale' scaleFactor points = map (mulSV scaleFactor) points
+scale' scaleFactor= map (mulSV scaleFactor)
 
 rotate' :: Float->[Vector]->[Vector]
-rotate' angle points = map (rotateV angle) points
+rotate' = map .  rotateV
 
 
 makePath :: LSystem->Integer->Path
-makePath (LSystem (Angle theta) (Axiom a) rules) n  = [(0,0)] ++ makePath' (iterate' n a rules) theta (0,0) 0
+makePath (LSystem (Angle theta) (Axiom a) rules) n  = (0,0) :  makePath' (iterate' n a rules) theta (0,0) 0
 
 makePath' :: String->Float->Vector->Float->Path
 makePath' [] theta curPos initialTheta = []
 makePath' (c:cs) theta curPos initialTheta
         | c == '+' = makePath' cs theta curPos (initialTheta + theta)
         | c == '-' = makePath' cs theta curPos (initialTheta - theta)
-        | otherwise = [newPos] ++ (makePath' cs theta newPos initialTheta)
+        | otherwise = newPos : makePath' cs theta newPos initialTheta
                 where newPos =  curPos + (sin initialTheta, cos initialTheta)
 
 iterate' :: Integer->String->[Rule]->String
@@ -74,27 +99,27 @@ iterate' 0 string rules = string
 iterate' n string rules = iterate' (n-1) (replace string rules) rules  
 
 replace :: String->[Rule]->String
-replace (c:[]) rules = replace' c rules
+replace [c] rules = replace' c rules
 replace (c:cs) rules = replace' c rules ++ replace cs rules
 
 replace' :: Char->[Rule]->String
-replace' c ((Rule c' s):[]) 
+replace' c [Rule c' s]
         | c == c'   = s
         | otherwise = [c]
-replace' c ((Rule c' s):rs)
+replace' c (Rule c' s:rs)
         | c == c'   = s
         | otherwise = replace' c rs 
 
 getScaleFactor :: LSystem->Float
-getScaleFactor system = (magV delta1)/(magV delta0)
-    where delta0 = (last path0)
-          delta1 = (last path1)
+getScaleFactor system = magV delta1/magV delta0
+    where delta0 = last path0
+          delta1 = last path1
           path0 = makePath system 0
           path1 = makePath system 1
 
 getTwistAngle :: LSystem->Angle
-getTwistAngle system  = Angle ((argV sum0) - (argV sum1))
-    where sum0 = (last path0) - (head path0)
-          sum1 = (last path1) - (head path1)
+getTwistAngle system  = Angle (argV sum0 - argV sum1)
+    where sum0 = last path0 - head path0
+          sum1 = last path1 - head path1
           path0 = makePath system 0
           path1 = makePath system 1
